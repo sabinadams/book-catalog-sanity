36b4e2cc54c39b734fdac1a2d2d6a4ad597d14c4
Sabin Adams <sabintheworld@gmail.com>
2022-08-22 12:58:16 -0700

    Initial Commit

---
+++ schemas/book.js
@@ -0,0 +1,22 @@
+export default {
+  name: "book",
+  type: "document",
+  title: "Book",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "author",
+      type: "string",
+      title: "Author",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Description",
+    },
+  ],
+};
---
+++ schemas/schema.js
@@ -0,0 +1,9 @@
+import createSchema from "part:@sanity/base/schema-creator";
+
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import book from "./book";
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes.concat([book]),
+});
